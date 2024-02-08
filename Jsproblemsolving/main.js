53c56901586b81237edbbf2de26c73cfa07f71e2
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-02-08 23:19:16 +0530

    sum

--- Jsproblemsolving/main.js
+++ Jsproblemsolving/main.js
@@ -1,9 +1,16 @@
    
 //     print 1 to 10 numbers
-   let num=10
+//    let num=10
 
-    for( let i=1;i<=num;i++){
-      console.log(i)
-    }
+//     for( let i=1;i<=num;i++){
+//       console.log(i)
+//     }
+ 
 
+//  add two number by arrawofunction
+
+ let sum=(a,b)=>{
+      console.log(a+b)
+ }
     
+ sum(10,50)
