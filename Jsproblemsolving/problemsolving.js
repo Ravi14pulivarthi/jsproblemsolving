e4dd27f68bd6027ddd37f881a3e7417a9fb128be
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-02 16:13:19 +0530

    two numbers argements

--- Jsproblemsolving/problemsolving.js
+++ Jsproblemsolving/problemsolving.js
@@ -11,8 +11,58 @@
       
 //2.Write a function that takes an integer minutes and converts it to seconds.
 
-  function time(mits){
-     console.log(mits*60)
-  }
-   time(1)
+//   function time(mits){
+//      console.log(mits*60)
+//   }
+//    time(1)
+
+ //*************************************problem1****************************
+//4.Create a function that takes the age in years and returns the age in days.
+
+
+//  function person(age){
+//       return(age *365)
+//  }
+
+//   console.log()
+ //*************************************problem1****************************
+ //6.Write a function that returns the string "something" joined with a space " " and the given argument a.
+
+//   function string(strings){
+
+//  console.log("Good moring "+strings)
+//   }
+
+//    string("Ravi")
+
+ //*************************************problem7****************************
+
+//7.Create a function that takes two arguments. Both arguments are integers, a and b. Return true if one of them is 10 or if their sum is 
+
+ let a=5
+  let b=5
+
+
+   function data(){
+   
+    if( a == 10) {
+        return true
+    }
+    if( b == 10) {
+        return true
+    }
+    if( a+b == 10) {
+        return true
+    }
+    else{
+         return false
+    }
+   }
+   console.log(  data())
+
+
+
+
+
+
 
