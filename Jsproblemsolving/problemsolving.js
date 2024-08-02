5af2029c606aa67d05838939261ef9d4bbd5f79e
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-02 15:16:00 +0530

    convert mits into senconds

--- Jsproblemsolving/problemsolving.js
+++ Jsproblemsolving/problemsolving.js
@@ -1,7 +1,18 @@
 //*************************************problem1***************************
 //1.Create a function that takes two numbers as arguments and returns their sum.
 
-   function add(a,b){
-    console.log(a+b)
-   }
-    add(10,20)
+//    function add(a,b){
+//     console.log(a+b)
+//    }
+//     add(10,20)
+
+     //*************************************problem1****************************
+
+      
+//2.Write a function that takes an integer minutes and converts it to seconds.
+
+  function time(mits){
+     console.log(mits*60)
+  }
+   time(1)
+
