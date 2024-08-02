a569998cb43f5505f68dc4af3edab5209f16dbbf
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-02 15:13:11 +0530

    add two numbers by function

---
+++ Jsproblemsolving/problemsolving.js
@@ -0,0 +1,7 @@
+//*************************************problem1***************************
+//1.Create a function that takes two numbers as arguments and returns their sum.
+
+   function add(a,b){
+    console.log(a+b)
+   }
+    add(10,20)
