6ec496b6d8290965a238891701d78b317f892649
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-06 17:21:21 +0530

    user inter 2 numbers

---
+++ Jsproblemsolving/jsmainproblem.js
@@ -0,0 +1,8 @@
+ let num1=prompt("enter a frist number")
+  let num2= prompt("enter a second number ")
+
+
+  let num3=parseInt(num1)
+   let num4=parseInt(num2)
+
+    console.log(num3+num4)
--- Jsproblemsolving/problemsolving.js
+++ Jsproblemsolving/problemsolving.js
@@ -451,14 +451,14 @@
 
      //*************************************problem*****************************************
 
- let names="ravi kumar pulivarthi mca"
+//  let names="ravi kumar pulivarthi mca"
 
 
-  let str2=names.split(" ")
-  let str3=""
+//   let str2=names.split(" ")
+//   let str3=""
 
-   for( let i=0;i<str2.length;i++){
-      str3+=str2[i].replace(str2[i][0],str2[i][0].toUpperCase())+" "
-   }
-       console.log(str3)
+//    for( let i=0;i<str2.length;i++){
+//       str3+=str2[i].replace(str2[i][0],str2[i][0].toUpperCase())+" "
+//    }
+//        console.log(str3)
 
