f1d270efaa665db139876a11c7a05ad6ab755556
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-06 17:31:06 +0530

    circle radius

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1,8 +1,20 @@
- let num1=prompt("enter a frist number")
-  let num2= prompt("enter a second number ")
+//  let num1=prompt("enter a frist number")
+//   let num2= prompt("enter a second number ")
 
 
-  let num3=parseInt(num1)
-   let num4=parseInt(num2)
+//   let num3=parseInt(num1)
+//    let num4=parseInt(num2)
 
-    console.log(num3+num4)
+//     console.log(num3+num4)
+
+
+
+ //************************** foren heat ***********************************
+
+
+
+
+ function circle(radius){
+     return ( 22/7 * radius* radius)
+ }
+  console.log( circle(4))
