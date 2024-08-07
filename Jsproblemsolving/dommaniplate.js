9c4713c7a5eb3ed66af9df34e60843cf200fe38a
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-07 11:32:53 +0530

    changing the text by dom

---
+++ Jsproblemsolving/dommaniplate.js
@@ -0,0 +1,11 @@
+ let title=document.getElementById("title");
+  let  changetextbutton= document.getElementById('changetextbutton')
+
+
+
+
+
+
+  changetextbutton.addEventListener("click",function(){
+     title.textContent='Text changed!'
+  })
