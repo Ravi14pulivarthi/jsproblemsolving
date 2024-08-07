7fb97ddba54d8351f87393f0fa58f0847b064c93
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-07 12:36:51 +0530

    dom manutplation

--- Jsproblemsolving/dommaniplate.js
+++ Jsproblemsolving/dommaniplate.js
@@ -1,5 +1,15 @@
- let title=document.getElementById("title");
-  let  changetextbutton= document.getElementById('changetextbutton')
+const  title=document.getElementById("title");
+  const changetextbutton= document.getElementById('changetextbutton')
+  const  butbackground=document.getElementById('butbackground')
+ let additem=document.getElementById("additem")
+ let removeitem=document.getElementById("removeitem")
+ let tooglebut=document.getElementById("tooglebut")
+  let clonebut=document.getElementById("clonebut")
+
+
+
+
+
 
 
 
@@ -8,4 +18,39 @@
 
   changetextbutton.addEventListener("click",function(){
      title.textContent='Text changed!'
+    
+  })
+
+
+  butbackground.addEventListener("click", function(){
+    title.style.color = 'blue';
+  })
+
+
+  additem.addEventListener("click",function(){
+     let newitem=document.createElement("li")
+     newitem.textContent="new item2"
+     itemlist.appendChild(newitem)
+  })
+
+
+  removeitem.addEventListener("click",function(){
+     itemlist.removeChild(itemlist.lastElementChild)
+  })
+
+
+  tooglebut.addEventListener("click",function(){
+     if(title.style.display ==="none"){
+         title.style.display ="block"
+     }
+      else{
+        title.style.display ="none"
+      }
+  })
+
+
+
+  clonebut.addEventListener("click", function(){
+    const fristItem=itemlist.firstElementChild.cloneNode(true)
+     itemlist.appendChild(fristItem)
   })
