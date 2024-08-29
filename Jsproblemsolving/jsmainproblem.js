76045214a820dc027a2c4438c50f98de8bad52e7
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-08-29 10:17:34 +0530

    charcount

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -984,17 +984,38 @@
    //***************************************** problem ***************************************************
   //  Alternate string in uppercase
 
-   let string='ravi kumar is a good boy'
+//    let string='ravi kumar is a good boy'
 
 
 
-   let string2=string.split(" ")
- let string3=''
+//    let string2=string.split(" ")
+//  let string3=''
 
-    for( let i=0;i<string2.length;i++){
-      string3+=string2[i].replace(string2[i][0] , string2[i][0].toLocaleUpperCase())+ " "
-    }
-     console.log(string3)
+//     for( let i=0;i<string2.length;i++){
+//       string3+=string2[i].replace(string2[i][0] , string2[i][0].toLocaleUpperCase())+ " "
+//     }
+//      console.log(string3)
+
+   //***************************************** problem ***************************************************
+
+    let string="raavi"
+
+
+     let charcount={}
+
+      for( let i=0;i<string.length;i++){
+         let count=string[i]
+
+
+           if(charcount[count]){
+            charcount[count]++
+           }
+            else{
+               charcount[count]=1
+            }
+      }
+       console.log(charcount)
+
 
 
 
