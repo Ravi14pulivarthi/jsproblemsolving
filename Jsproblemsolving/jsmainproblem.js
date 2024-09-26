ce1b8adfebb1ca1842e10aabdd0ba09d3399b351
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-09-26 09:59:38 +0530

    desnding order

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1269,7 +1269,7 @@
                                   for( i=0;i<array.length;i++){
                               
                                       for( let j=0;j<array.length;j++){
-                                         if( array[i] < array[j]){
+                                         if( array[i] > array[j]){
                                           swap=array[i]
                                           array[i]=array[j]
                                           array[j]=swap
