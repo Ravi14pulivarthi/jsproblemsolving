0a109bbd5ef4316870e8b386ebf6f13f87ea4b8b
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-10-03 15:12:30 +0530

    largest num

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1439,10 +1439,10 @@
     let swap=" "
      for( let i=0;i<nums.length;i++){
        for( let j=0;j<nums.length;j++){
-           if( nums[i] > nums[j]){
+           if( nums[i] < nums[j]){
             swap = nums[i]
             nums[i]=nums[j]
-            nums[j]=" "
+            nums[j]=swap
             
            }
        }
