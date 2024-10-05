d5624c1efa1c396a1a8536060fadce63b0b29cc3
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-10-05 15:30:40 +0530

    array 1 men change the placein index

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1476,10 +1476,10 @@
 
       //  array methods
 
-       let array=[1,2,3,4,5,6,7,"ravi",'kumar']
+      let array=[1,2,3,4,5,6,7,"ravi",'kumar']
 
-          let arr2=array.splice(0,1,10)
-           console.log(array)
+      let arr2=array.splice(0,1,10)
+       console.log(array)
 
 
 
