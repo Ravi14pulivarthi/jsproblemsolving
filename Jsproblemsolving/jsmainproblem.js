cf8335eb23775f33bed17769a0698796dfae25fa
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-10-05 15:38:14 +0530

    to string

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1476,10 +1476,10 @@
 
       //  array methods
 
-      let array=[1,2,3,4,5,6,7,"ravi",'kumar']
+      let array=["ravi",'kumar']
 
-      let arr2=array.push("pulivarthi")
-       console.log(array)
+      
+       console.log(array.toString())
 
 
 
