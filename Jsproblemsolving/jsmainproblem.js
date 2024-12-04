35062d0664f83dbec73debca33e9e2cc9d8653c0
Ravi14pulivarthi <pulivarthiravi2959@gmail.com>
2024-12-04 11:57:10 +0530

    desding order

--- Jsproblemsolving/jsmainproblem.js
+++ Jsproblemsolving/jsmainproblem.js
@@ -1842,7 +1842,7 @@
    for( let i=0;i<nums.length;i++){
 
        for( let j=0;j<nums.length;j++){
-          if(nums[i] <nums [j]){
+          if(nums[i] > nums [j]){
             swap = nums[i]
             nums[i]=nums[j]
              nums[j]= swap
